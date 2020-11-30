0763c1266f67ae2e62d88a5d1041ab1ed4357057
Nick42791 <nmoran2@asu.edu>
2020-11-30 00:05:12 -0700

    javadoc comments

--- main/controller/ClickableOptions/AddAttendanceOption.java
+++ main/controller/ClickableOptions/AddAttendanceOption.java
@@ -1,17 +1,41 @@
+/**
+ * The 'Add attendance' menu option
+ * 
+ * @author Shakib Ahmed, Nick Moran, Madeleine Householder
+ * @version 1.0
+ * @since 2020-11-29
+ */
 package main.controller.ClickableOptions;
 
 import java.awt.event.ActionEvent;
 
-import javax.swing.AbstractAction;
-
 import main.controller.ClickableItem;
 import main.model.StateManager;
 
+/**
+ * AddAttendanceOption extends ClickableItem, so it is a 
+ * listener. it updates the state to move to
+ * the home page and then run the add attendance option.
+ * 
+ * @author Shakib Ahmed, Nick Moran, Madeleine Householder
+ */
 public class AddAttendanceOption extends ClickableItem{
+	/**
+	 * Makes the name of the option add attendance. 
+	 * and maps the state object to the parent.
+	 * @param state
+	 */
 	public AddAttendanceOption(StateManager state) {
 		super(state, "Add attendance");
 	}
 	
+	/**
+	 * a realization of the parent actionPerformed method
+	 * updates the state to change the page and then
+	 * enter the add attendance section.
+	 * 
+	 * @param e
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		super.updateState("home");
--- main/controller/ClickableOptions/LoadRosterOption.java
+++ main/controller/ClickableOptions/LoadRosterOption.java
@@ -1,18 +1,40 @@
+/**
+ * The 'Load the Roster' menu option
+ * 
+ * @author Shakib Ahmed, Nick Moran, Madeleine Householder
+ * @version 1.0
+ * @since 2020-11-29
+ */
 package main.controller.ClickableOptions;
 
 import java.awt.event.ActionEvent;
-import java.util.ArrayList;
-
-import javax.swing.AbstractAction;
 
 import main.controller.ClickableItem;
 import main.model.StateManager;
 
+/**
+* LoadRosterOption extends ClickableItem, so it is a 
+* listener. it updates the state to move to
+* the home page and run the the load roster option.
+* 
+* @author Shakib Ahmed, Nick Moran, Madeleine Householder
+*/
 public class LoadRosterOption extends ClickableItem{
+	/**
+	 * Makes the name of the option Load the Roster. 
+	 * and maps the state object to the parent.
+	 * @param state
+	 */
 	public LoadRosterOption(StateManager state) {
 		super(state, "Load the Roster");
 	}
 	
+	/**
+	 * a realization of the parent actionPerformed method
+	 * updates the state to change the page and then
+	 * enter the load roster section.
+	 * @param e
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		super.updateState("home");
